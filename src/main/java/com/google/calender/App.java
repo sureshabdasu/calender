ac655bf1550ae0c0bb8d3b1de698aae3a6059069
suresh.abdasu <suresh.abdasu@wavelabs.ai>
2021-09-03 12:10:50 +0530

    Modified App.java again

--- src/main/java/com/google/calender/App.java
+++ src/main/java/com/google/calender/App.java
@@ -11,6 +11,5 @@ public class App
     public static void main( String[] args )
     {
         System.out.println(WordUtils.capitalize( "hello world!" ));
-        System.out.println(WordUtils.capitalize( "hello world!" ));
     }
 }
